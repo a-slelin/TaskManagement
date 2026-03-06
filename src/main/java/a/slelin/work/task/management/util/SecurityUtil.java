78dbfec0662e490917d35fa216d5bf10b9071f51
Alexandr Slelin <a.slelin.work@mail.ru>
2026-03-06 18:25:41 +0300

    add: security for password

---
+++ src/main/java/a/slelin/work/task/management/util/SecurityUtil.java
@@ -0,0 +1,17 @@
+package a.slelin.work.task.management.util;
+
+import lombok.AccessLevel;
+import lombok.NoArgsConstructor;
+import org.mindrot.bcrypt.BCrypt;
+
+@NoArgsConstructor(access = AccessLevel.PRIVATE)
+public final class SecurityUtil {
+
+    public static String hashPassword(String password) {
+        return BCrypt.hashpw(password, BCrypt.gensalt());
+    }
+
+    public static boolean checkPassword(String password, String hashedPassword) {
+        return BCrypt.checkpw(password, hashedPassword);
+    }
+}
