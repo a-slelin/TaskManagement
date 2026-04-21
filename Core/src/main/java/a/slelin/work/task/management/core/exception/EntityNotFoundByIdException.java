65d6bd1ab1034a7b87bc5077cb918bccc145d14c
Alexandr Slelin <a.slelin.work@mail.ru>
2026-04-21 13:11:21 +0300

    add: exceptions in core

---
+++ Core/src/main/java/a/slelin/work/task/management/core/exception/EntityNotFoundByIdException.java
@@ -0,0 +1,23 @@
+package a.slelin.work.task.management.core.exception;
+
+import lombok.Getter;
+
+@Getter
+public class EntityNotFoundByIdException extends RuntimeException {
+
+    private final Class<?> entity;
+
+    private final Object invalidKey;
+
+    @SuppressWarnings("unused")
+    public EntityNotFoundByIdException(Class<?> entity, Object invalidKey) {
+        this(entity, invalidKey, "Entity '%s' not found by id = '%s'."
+                .formatted(entity.getSimpleName(), invalidKey.toString()));
+    }
+
+    public EntityNotFoundByIdException(Class<?> entity, Object invalidKey, String message) {
+        this.entity = entity;
+        this.invalidKey = invalidKey;
+        super(message);
+    }
+}
